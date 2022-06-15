346539068206742d4db0c068233854fb963298a4
cullnan <58110495+cullnan@users.noreply.github.com>
2022-06-15 21:16:23 +0600

    Update Tasks.js

--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -5,23 +5,21 @@ import workspaceStore from "../data/stores/workspaceStore";
 const handleClickTask = (workIndex, deskIndex, categoryIndex, item) =>{
     console.log(workIndex, deskIndex, categoryIndex);
     workspaceStore.addTask(workIndex, deskIndex, categoryIndex, item)
-    workspaceStore.setSelectWorkspaceIndex(workIndex)    
-    workspaceStore.selectedDesk = workspaceStore.workspaces[workIndex].desks[deskIndex].title
 }
 
 
-const Tasks = observer(({categoryIndex}) => {
+const Tasks = observer(({sWorkspace, sDesk, categoryIndex}) => {
     const tasksArr = []
 
     console.log("categoryIndex: ", categoryIndex );
-    if(workspaceStore.selectedWorkspace.desks.length != 0 && workspaceStore.selectedWorkspace.desks[0].categories.length != 0 && workspaceStore.selectedWorkspace.desks[0].categories[categoryIndex].tasks.length != 0){
-        for(let i = 0; i < workspaceStore.selectedWorkspace.desks[workspaceStore.getDeskIndex(workspaceStore.selectedDesk)].categories[categoryIndex].tasks.length; i++){
+    if(workspaceStore.workspaces[sWorkspace].desks.length != 0 && workspaceStore.workspaces[sWorkspace].desks[sDesk].categories.length != 0 && workspaceStore.workspaces[sWorkspace].desks[sDesk].categories[categoryIndex].tasks.length != 0){
+        for(let i = 0; i < workspaceStore.workspaces.find(item => item.id == workspaceStore.selectedWorkspace).desks.find(item => item.id == workspaceStore.selectedDesk).categories[categoryIndex].tasks.length; i++){
             tasksArr.push(
             <div className="task">
-                <input type="text" name="" id="" value={workspaceStore.selectedWorkspace.desks[workspaceStore.getDeskIndex(workspaceStore.selectedDesk)].categories[categoryIndex].tasks[i].title}
+                <input type="text" name="" id="" value={workspaceStore.workspaces[sWorkspace].desks[sDesk].categories[categoryIndex].tasks[i].title}
                 onChange={(event) => {
-                    workspaceStore.setTaskTitle(workspaceStore.getWorkIndex(workspaceStore.selectedWorkspace.title),
-                    workspaceStore.getDeskIndex(workspaceStore.selectedDesk), categoryIndex,i ,event.target.value)
+                    workspaceStore.setTaskTitle(sWorkspace,
+                    sDesk, categoryIndex,i ,event.target.value)
                     }}
                 />
             </div>
@@ -34,7 +32,7 @@ const Tasks = observer(({categoryIndex}) => {
             {
                 tasksArr
             }
-            <button className="add_task" onClick={() => handleClickTask(workspaceStore.getWorkIndex(workspaceStore.selectedWorkspace.title), workspaceStore.getDeskIndex(workspaceStore.selectedDesk), categoryIndex, {title: "New task"})}>+</button>
+            <button className="add_task" onClick={() => handleClickTask(sWorkspace, sDesk, categoryIndex, {id: Date.now(), title: "New task"})}>+</button>
         </div>
     )
 });
