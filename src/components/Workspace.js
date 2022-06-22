1198ac992915b5438179d75544a507389adeea7d
cullnan <58110495+cullnan@users.noreply.github.com>
2022-06-22 16:01:43 +0600

    Update Workspace.js

--- src/components/Workspace.js
+++ src/components/Workspace.js
@@ -1,4 +1,5 @@
 import { observer } from "mobx-react-lite";
+import { BgColorsOutlined, EllipsisOutlined, DeleteOutlined } from '@ant-design/icons';
 import workspaceStore from "../data/stores/workspaceStore";
 
 
@@ -8,15 +9,15 @@ export const Workspace = observer(() => {
         <div className="workspaces">
               {
                 workspaceStore.workspaces.map(item => (
-                <div className="workspace"
+                <div className="workspace" >
+                  <div className="workspace_img"><img src={item.img || null} alt={item.title[0]}/></div>
+                  <div className="workspace_title" 
                  onClick={() => {
                    workspaceStore.selectedWorkspace = item.id
                    workspaceStore.selectedDesk = workspaceStore.workspaces[workspaceStore.workspaces.findIndex(ws => ws.id == workspaceStore.selectedWorkspace)].desks[0].id
                    console.log({img: item.img, title:item.title, desks: item.desks});
-                  }}
-                >
-                  <div className="workspace_img"><img src={item.img || null} alt={item.title[0]}/></div>
-                  <div className="workspace_title"> {item.title} </div>
+                  }}> {item.title} </div>
+                  <div className="delete_icon" onClick={() => workspaceStore.deleteWorkspace(item.id)}><DeleteOutlined style={{ fontSize: '25px'}}/> </div>
                 </div>
                 ))
               }
