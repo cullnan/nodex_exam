dbd4e9e04712de7989bc42607f1baa89b306f064
cullnan <58110495+cullnan@users.noreply.github.com>
2022-07-14 21:46:50 +0600

    Update MainPage.js

--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -5,11 +5,17 @@ import Popup from '../components/Popup';
 import { observer } from "mobx-react-lite";
 import { useState } from 'react';
 import Category from "../components/Categories";
-import { Input } from 'antd';
+import { AutoComplete } from 'antd';
+const { Option } = AutoComplete;
 
 export const MainPage = observer(() => {
  
     const [popupActive, setPopupActive] = useState(false);
+
+    const onSelect = (data) => {
+        workspaceStore.selectedWorkspace = workspaceStore.options.find(item => item.title === data).idWork
+        workspaceStore.selectedDesk = workspaceStore.options.find(item => item.title === data).idDesk
+      };
 
     return(
         <div className="app">
@@ -23,7 +29,22 @@ export const MainPage = observer(() => {
                 </div>
             </div>
 
-            <Input placeholder="Basic usage" onChange={() => workspaceStore.searchTask('купи')}/>
+            <AutoComplete
+                style={{
+                width: '100%',
+                }}
+                onSelect={(data) => {
+                    onSelect(data)
+                    this.value = ""
+                }}
+                onSearch={(e) => workspaceStore.searchTask(e)}
+                placeholder="input here">
+                {workspaceStore.options.map((item) => (
+                    <Option key={item.title} value={item.title}>
+                      {item.title}
+                    </Option>
+                  ))}
+            </AutoComplete>
             {workspaceStore.workspaces.length > 0 ? <Workspace /> : <></>}
 
             <div className="add">
@@ -36,7 +57,7 @@ export const MainPage = observer(() => {
         <div className="main_block">
             <div className="header">
                 <div className="workspace_in_main">
-                    <div className="workspace_img"><img src={workspaceStore.workspaces.find(item => item.id == workspaceStore.selectedWorkspace).img || null} alt={workspaceStore.workspaces.find(item => item.id == workspaceStore.selectedWorkspace).title[0]}/></div>
+                    <div className="workspace_img"><img src={workspaceStore.workspaces.find(item => item.id === workspaceStore.selectedWorkspace).img || "https://plasticsurgery-ua.org/wp-content/uploads/2016/11/default-placeholder.png"} /></div>
                     <div className="workspace_title">{workspaceStore.workspaces.length > 0 ? workspaceStore.workspaces.find(item => item.id == workspaceStore.selectedWorkspace).title : <></>}  </div>
                 </div>
 
